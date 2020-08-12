c175a1e97fabefa3a96a4f366cb2bc847ad299f7
Johannes Vetter <johannes@art19.com>
2020-08-11 18:35:35 -0700

    Provide our custom build script

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+error() {
+  echo $1
+  exit 1
+}
+
+test -z "${VERSION}"         && error "VERSION is required"
+test -z "${DOCKER_USERNAME}" && error "DOCKER_USERNAME is required"
+test -z "${DOCKER_PASSWORD}" && error "DOCKER_PASSWORD is required"
+
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-docker build -t netuitive/docker-agent .;
-docker tag netuitive/docker-agent netuitive/docker-agent:$TRAVIS_TAG;
-docker push netuitive/docker-agent:latest;
-docker push netuitive/docker-agent:$TRAVIS_TAG;
+docker build -t art19/cloudwisdom-dedicated-collector .;
+docker tag art19/cloudwisdom-dedicated-collector art19/cloudwisdom-dedicated-collector:$VERSION;
+docker push art19/cloudwisdom-dedicated-collector:latest;
+docker push art19/cloudwisdom-dedicated-collector:$VERSION;
