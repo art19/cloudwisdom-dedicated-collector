d9eae8687532388784b8a463ddd9ad947aa18fc8
Johannes Vetter <johannes@art19.com>
2020-08-11 19:01:02 -0700

    Get rid of explicit docker-login

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -5,11 +5,8 @@ error() {
   exit 1
 }
 
-test -z "${VERSION}"         && error "VERSION is required"
-test -z "${DOCKER_USERNAME}" && error "DOCKER_USERNAME is required"
-test -z "${DOCKER_PASSWORD}" && error "DOCKER_PASSWORD is required"
+test -z "${VERSION}" && error "VERSION is required"
 
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
 docker build -t art19/cloudwisdom-dedicated-collector .;
 docker tag art19/cloudwisdom-dedicated-collector art19/cloudwisdom-dedicated-collector:$VERSION;
 docker push art19/cloudwisdom-dedicated-collector:latest;
